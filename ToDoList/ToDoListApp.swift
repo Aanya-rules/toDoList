5fb61a6456185e00f1d8770d560d56887fbcef75
Aanya Mahapatra <aanya.mahapatra@gmail.com>
2025-07-14 08:57:45 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Aanya Mahapatra on 14/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
